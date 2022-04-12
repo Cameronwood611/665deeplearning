e6c5357c8a8a8050b0f553357b163865246d021d
Jan Foksinski <jkf@uab.edu>
2022-04-11 19:01:33 -0500

    Initial Webapp code push

---
+++ math-buddy/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { Layout, Row, Col } from "antd";
+import { SearchForm } from "./SearchForm";
+
+const { Header, Content, Footer } = Layout;
+
+function App() {
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <h1 style={{ color: "white" }}>Math Buddy</h1>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Layout
+          className="site-layout-background"
+          style={{ padding: "24px 0" }}
+        >
+          <Content
+            style={{ padding: "0 24px", minHeight: 280, height: "100%" }}
+          >
+            <Row>
+              <Col xs={24} xl={8}></Col>
+              <Col xs={24} xl={8}>
+                <SearchForm />
+              </Col>
+              <Col xs={24} xl={8}></Col>
+            </Row>
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: "center" }}></Footer>
+    </Layout>
+  );
+}
+
+export default App;
