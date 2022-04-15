0adc3595ee6af7c8bd5e0c3d03cb03a78a7c953e
Cameronwood611 <cwood611@uab.edu>
2022-04-15 00:32:54 -0500

    load model, predict and send yt link

--- math-buddy/public/routes.py
+++ math-buddy/public/routes.py
@@ -1,17 +1,79 @@
+import math
 import os
 
+import cv2
 # PDM
-from flask import Flask, send_file, request, jsonify
+import numpy as np
+from flask import Blueprint, Flask, jsonify, request, send_file
+from tensorflow.keras.applications import resnet50
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
-app = Flask(__name__)
+bp = Blueprint("routes", __name__)
+
+def get_yt(type: str):
+    videos = {
+        "matrix": "https://www.youtube.com/embed/0oGJTQCy4cQ"
+    }
+    return videos[type]
 
 
-@app.route("/upload", methods=["POST"])
+def resize_img(im: np.ndarray, dim: tuple):
+    y, x = im.shape[0], im.shape[1]
+    y_out, x_out = dim
+
+    if y_out > y:
+        diff = abs(y_out - y)
+        padding = math.ceil(diff / 2)
+        im = cv2.copyMakeBorder(im, padding, padding, 0, 0, cv2.BORDER_CONSTANT, None, value=[255, 255, 255])
+
+    if x_out > x:
+        diff = abs(y_out - y)
+        padding = math.ceil(diff / 2)
+        im = cv2.copyMakeBorder(im, 0, 0, padding, padding, cv2.BORDER_CONSTANT, None, value=[255, 255, 255])
+    
+    im = cv2.resize(im, dim, interpolation= cv2.INTER_CUBIC)
+    return im
+
+
+def get_image(filename):
+    original = load_img(filename, target_size=(224,224))
+    numpy_image = img_to_array(original)
+    image_batch = np.expand_dims(numpy_image, axis=0)
+    return image_batch[0]
+
+
+def predict(file: str):
+    im = cv2.imread(file)
+    im = resize_img(im, (224, 224))
+    model = load_model("../saved_models/model_hinge.h5")
+    types = ['distribute', 'ineq', 'integral', 'limit', 'matrix', 'series', 'sqrt']
+
+    resnet_mse_model = resnet50.ResNet50(weights='imagenet', include_top=False)
+    test_img = get_image(file)
+    image_batch = np.expand_dims(test_img, axis=0)
+    image_batch = np.copy(image_batch)
+    image_batch = resnet50.preprocess_input(image_batch)
+    feature_input = resnet_mse_model.predict(image_batch)
+    predictions = model.predict(feature_input)
+    pos = np.argmax(predictions)
+    pred = types[pos]
+    return pred
+
+
+
+@bp.route("/upload", methods=["POST"])
 def upload():
+    print("yeet")
     if request.method == "POST":
         f = request.files["file"]
         if f.filename:
-            f.save(os.path.join("./uploaded_files", f.filename))
+            os.makedirs("./uploaded_files", exist_ok=True)
+            path = os.path.join("./uploaded_files", f.filename)
+            f.save(path)
+            math_type = predict(path)
+            return get_yt(math_type)
+
     return "{}"
 
 
@@ -25,12 +87,12 @@ def upload():
 #     return ""
 
 
-@app.route("/favicon.ico")
-@app.route("/static/images/favicon.ico")  # because other apps have the favicon here
+@bp.route("/favicon.ico")
+@bp.route("/static/images/favicon.ico")  # because other apps have the favicon here
 def favicon():
     return send_file("./favicon.ico")
 
 
-@app.route("/")
+@bp.route("/")
 def home():
     return send_file("index.html")
